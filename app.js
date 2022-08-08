0f01da61742e2e39f048c1f3994dce0f5cc09fbb
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2022-08-08 20:07:36 +0200

    Solved Lab

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static("pulic")) >> doesn't work here
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+})
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(__dirname + '/views/gallery.html')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
